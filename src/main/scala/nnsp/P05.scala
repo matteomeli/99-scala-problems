55447162988f85b8875b5066af4af60d4fe62d3a
Matteo Meli <matteo.meli@gmail.com>
2016-11-13 21:16:50 +0000

    Add missing tailrec annotation

--- src/main/scala/nnsp/P05.scala
+++ src/main/scala/nnsp/P05.scala
@@ -1,7 +1,10 @@
 package nnsp
 
+import scala.annotation.tailrec
+
 object P05 {
   def reverse[A](list: List[A]): List[A] = {
+    @tailrec
     def reverseRec(result: List[A], current: List[A]): List[A] = current match {
       case head :: tail => reverseRec(head :: result, tail)
       case _ => result
