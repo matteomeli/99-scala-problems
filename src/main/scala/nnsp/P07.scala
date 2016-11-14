a33eeaaa608f93b8be9d7b4fd8fc5db7fb2e25fa
Matteo Meli <matteo.meli@gmail.com>
2016-11-14 18:06:13 +0000

    Improve flatten function to be tail recursive

--- src/main/scala/nnsp/P07.scala
+++ src/main/scala/nnsp/P07.scala
@@ -1,9 +1,16 @@
 package nnsp
 
+import scala.annotation.tailrec
+
 object P07 {
-  def flatten(list: List[Any]): List[Any] = list match {
-    case (head : List[_]) :: tail => flatten(head) ::: flatten(tail)
-    case head :: tail => head :: flatten(tail)
-    case _ => Nil
+  def flatten(list: List[Any]): List[Any] = {
+    @tailrec
+    def flattenRec(result: List[Any], current: List[Any]): List[Any] = current match {
+      case (head : List[_]) :: Nil => flattenRec(result, head)
+      case (head : List[_]) :: tail => flattenRec(result ::: head, tail)
+      case head :: tail => flattenRec(result ::: List(head), tail)
+      case _ => result
+    }
+    flattenRec(List(), list)
   }
 }
