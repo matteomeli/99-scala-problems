b225d2c1169a5ac6037dae82d35a8812e4e21c01
Matteo Meli <matteo.meli@gmail.com>
2016-11-17 23:13:37 +0000

    Add missing tailrec annotation

--- src/main/scala/nnsp/P10.scala
+++ src/main/scala/nnsp/P10.scala
@@ -5,6 +5,7 @@ import P09._
 
 object P10 {
   def encode[A](list: List[A]): List[(Int, A)] = {
+    @annotation.tailrec
     def loop(l: List[List[A]], result: List[(Int, A)]): List[(Int, A)] = l match {
       case Nil => reverse(result)
       case x :: xs => loop(xs, (x.length, x.head) :: result)
