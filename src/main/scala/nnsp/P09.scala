cc79850fb900118196d56af54e4f57151a67ef02
Matteo Meli <matteo.meli@gmail.com>
2016-11-17 22:24:22 +0000

    Simplify solution to problem 9

--- src/main/scala/nnsp/P09.scala
+++ src/main/scala/nnsp/P09.scala
@@ -5,13 +5,11 @@ import P05._
 object P09 {
   def pack[A](list: List[A]): List[List[A]] = {
     @annotation.tailrec
-    def loop(result: List[List[A]], current: List[A], rest: List[A]): List[List[A]] = (current, rest) match {
-      case (Nil, Nil) => result
-      case (_ :: _, Nil) => current :: result
-      case (Nil, y :: ys) => loop(result, List(y), ys)
-      case (x :: _, y :: ys) if x == y => loop(result, y :: current, ys)
-      case (x :: _, y :: ys) => loop(current :: result, List(y), ys)
+    def loop(result: List[List[A]], rest: List[A]): List[List[A]] = (result, rest) match {
+      case ((h :: t) :: xs, y :: ys) if h == y => loop((h :: y :: t) :: xs, ys)
+      case (l, y :: ys) => loop(List(y) :: l, ys)
+      case _ => result
     }
-    reverse(loop(List(), List(), list))
+    reverse(loop(List(), list))
   }
 }
